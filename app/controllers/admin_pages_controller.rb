cb3cb369424b2441054c56f6ed826d38640abc79
rovf <ynnor@mm.st>
2014-09-02 11:56:10 +0200

    create password entry form for administrator

--- app/controllers/admin_pages_controller.rb
+++ app/controllers/admin_pages_controller.rb
@@ -2,8 +2,14 @@ class AdminPagesController < ApplicationController
   def adm_login_form
       render 'adm_login'
   end
-  
+
+  # params[:admpwd] contains password
   def adm_login
+      # authenticate. We first do only static comparision. Later, we allow
+      #   a password to be stored encrypted in the database.
+      # set session to admin or reject; display error message on reject
+      # render admin home or adm_login
+      flash.now[:error]='Ungültiges Administratorpasswort'
   end
 
   def adm_logout
