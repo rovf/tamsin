feb91f79233778248550aebbfb994e2f8e577ad8
rovf <ynnor@mm.st>
2015-01-09 17:27:42 +0100

    interim commit: add default creation for userpage

--- app/models/userpage.rb
+++ app/models/userpage.rb
@@ -1,2 +1,11 @@
 class Userpage < ActiveRecord::Base
+
+    validates :filename, presence: true, length: {minimum: 3}, format: {with: /[.]/}, uniqueness: true
+
+    validates :linkname, presence: true, length: {minimum: 1}
+
+    def self.new_page_with_default(fname)
+        Userpage.find_by_filename(fname).nil? ? Userpage.new(filename: fname, linkname: File.basename(fname,'.*'), seqno: 0, valid_from: nil, valid_to: nil) : nil
+    end
+
 end
